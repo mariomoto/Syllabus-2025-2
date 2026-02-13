70107dfd72f56d1f5bf16a9fb0cf53bd4d9767ae
Mario Amaro <amaro.mario@gmail.com>
2026-02-13 08:19:55 -0300

    AC1 completada.

--- Actividades/AC1/main.py
+++ Actividades/AC1/main.py
@@ -1,8 +1,31 @@
 import os
-from os import path
+import entities
+import utils.pretty_print as pp
 
-filepath = os.path.join(os.getcwd(), "utils", "items.dcc")
-print(filepath)
-file = open(filepath, "r", encoding="utf-8")
-for line in file:
-    print(line, end="")
+from os import path
+from entities import Item, Usuario
+
+def cargar_items() -> list:
+    filepath = os.path.join(os.getcwd(), "utils", "items.dcc")
+    file = open(filepath, "r", encoding="utf-8")
+    items = []
+    for line in file:
+        nombre, puntos, precio = line.strip().split(",")
+        items.append(Item(nombre, int(puntos), int(precio)))
+    file.close()
+    return items
+
+def crear_usuario(tiene_suscripcion: bool) -> Usuario:
+    usuario = Usuario(tiene_suscripcion)
+    pp.print_usuario(usuario)
+    return usuario
+
+if __name__ == "__main__":
+    usuario = crear_usuario(True)
+    items = cargar_items()
+    pp.print_items(items)
+    for item in items:
+        usuario.agregar_item(item)
+    pp.print_canasta(usuario)
+    usuario.comprar()
+    pp.print_usuario(usuario)
---
+++ Actividades/AC1/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+nombre, int(puntos), int(precio)
